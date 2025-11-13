ddaccc69e45389de9aa94303a66528ee4d404dc8
Kolmy <test@neto.com>
2025-11-13 22:51:02 +0300

    first commit

---
+++ src/main/java/ru/rksp/Kolmykova/NataliaApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Kolmykova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NataliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NataliaApplication.class, args);
+	}
+
+}
