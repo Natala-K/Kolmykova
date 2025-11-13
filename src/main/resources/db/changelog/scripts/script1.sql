6213713204cd49bbe20ab7d207e3cbc15c28ebcf
Kolmy <test@neto.com>
2025-11-13 23:44:44 +0300

    third commit

---
+++ src/main/resources/db/changelog/scripts/script1.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA utmn;
+CREATE TABLE utmn.student
+id bigserial NOT
+NULL,
+fio varchar(128) NOT
+);
+COMMENT ON TABLE utmn.student IS 'Таблица для хранения информации о
+судентах';
+COMMENT ON COLUMN utmn.student.fio IS 'ФИО';
